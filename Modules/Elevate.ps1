16b37afb46971f9efa25880fcb506a2aa923370a
Ruben Meneses <25863649+RubenMeneses@users.noreply.github.com>
2018-11-26 23:42:26 +1300

    deleted old files

--- Modules/Elevate.ps1
+++
@@ -1,7 +0,0 @@
-function Elevate 
-{
-    Write-Host "Not running with administrative rights. Attempting to elevate..."
-    $command = "-ExecutionPolicy bypass -noexit -command &'$ThisScriptFullPath'"
-    Start-Process powershell -verb runas -argumentlist $command
-    Exit
-}
